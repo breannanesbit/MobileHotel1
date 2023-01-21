881d795a2ac3218e93ef5aeaf949cf4f4d7ea5ba
Breanna.nesbit <Breanna.nesbit@ENGR6F8P5S3>
2023-01-20 17:20:53 -0700

    started adding binding and maui page for hotel room types

---
+++ HotelMobileApp/ViewModel/MainViewModel.cs
@@ -0,0 +1,31 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using HotelFinal.Shared;
+using MoblieShared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelMobileApp.ViewModel
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        private readonly HotelService service;
+        public MainViewModel(HotelService service)
+        {
+            this.service = service;
+        }
+
+        [ObservableProperty]
+        private List<RoomType> room;
+
+        [RelayCommand]
+        private async Task GetAllRoomTypes()
+        {
+            Room = await service.GetAllRoomTypesAsync();
+        }
+
+    }
+}
